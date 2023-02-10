72c4b354164176e6b5dcd1e33e5c7ea7030c971d
Vitaliy Semenov <synthex@bk.ru>
2023-02-10 23:50:31 +0600

    feat: GuessingGame class implemented

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
+      this.guessing = null;
     }
 
     guess() {
-
+      this.guessing = Math.round((this.max + this.min) / 2);
+      return this.guessing;
     }
 
     lower() {
-
+      this.max = this.guessing;
     }
 
     greater() {
-
+      this.min = this.guessing;
     }
 }
 
